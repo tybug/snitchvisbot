ba6c9a5e1afdd7da2aad50aabed2561fe8c2315e
Liam DeVoe <orionldevoe@gmail.com>
2022-08-01 15:47:46 -0400

    reword

--- command.py
+++ command.py
@@ -232,7 +232,7 @@ def channel(message, val):
     match = re.match(r"<#([0-9]+)>", val)
     if not match:
         raise ParseError(f"Invalid channel `{val}`. Make sure to use a proper "
-            "channel mention (ie `#channel`) instead of just the channel name.")
+            "channel mention (eg `#snitches`) instead of just the channel name.")
     channel_id = int(match.group(1))
     return message.guild.get_channel(channel_id)
 
